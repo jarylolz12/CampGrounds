31c2150ed0863ebf55191018ef182f482425e3d0
ZjeArr <jarylolz12@gmail.com>
2020-07-07 23:52:13 +0800

    adding to my github repo

--- app.js
+++ app.js
@@ -62,6 +62,8 @@ app.get('*', (req, res) => {
 
 //--------------------------------------------------------------------pag start sa server (localhost)
 
-app.listen(process.env.PORT, process.env.IP, () => {
-	console.log('Started');
+app.listen('3000', () => {
+	hostname = '127.0.0.1';
+	port = 3000;
+	console.log(`YelpCamp running at http://${hostname}:${port}/`);
 });
